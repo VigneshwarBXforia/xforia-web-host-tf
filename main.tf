917331f438dfb6127f9961bdab44ed1f6136a54d
Vigneshwar B <vigneshwar.balakrishnan@xforia.com>
2025-05-19 19:16:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,16 @@
-module "vm" {
-  source         = "./modules/compute_instance"
-  instance_name  = var.instance_name
-  zone           = var.zone
+resource "google_compute_instance" "default" {
+  name         = "xfoira-web-host"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-minimal-2210-kinetic-amd64-v20230126"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
 }
